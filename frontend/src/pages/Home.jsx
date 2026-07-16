49cc7a037311c488abe2671e01b6c10192053003
Nathan Abigail Rahman <supersekali1256@gmail.com>
2026-07-16 21:08:52 +0700

    fix: import useState to prevent blank screen crash

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 import ProjectCard from '../components/ProjectCard';
